d61617d204368f7b278313ba7cb8aae947e5b6af
wccarleton <ccarleton@protonmail.com>
2024-06-07 16:00:33 +0200

    more sphinx docs

--- chronocluster/utils.py
+++ chronocluster/utils.py
@@ -80,8 +80,7 @@ def clustering_heatmap(results,
     Plot a heatmap of either Ripley's K function or the pair correlation function over time and distance.
     
     Parameters:
-    results (np.ndarray): A 3D array where the first dimension is the distance, 
-                          the second dimension is the time slice, and the third dimension is the iteration.
+    results (np.ndarray): A 3D array where the first dimension is the distance, the second dimension is the time slice, and the third dimension is the iteration.
     time_slices (array-like): Array of time slices.
     distances (array-like): Array of distances at which K or g was calculated.
     result_type (str): The type of results being plotted ('K' for Ripley's K function or 'g' for pair correlation function).
